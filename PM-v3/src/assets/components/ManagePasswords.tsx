824bf044d3b46cc2e5ed2398007c2a7a1499a75d
Luis C <luis.cristovao92@gmail.com>
2025-04-15 15:48:59 +0100

    Update ManagePasswords.tsx

--- PM-v3/src/assets/components/ManagePasswords.tsx
+++ PM-v3/src/assets/components/ManagePasswords.tsx
@@ -1,17 +1,36 @@
 import { encrypt, decrypt, sha256 } from "../utils/cryptoUtils.ts";
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
 import { add, load, update, remove } from "../utils/dbUtils";
 
 
 
 function ManagePasswords() {
-  
+  const pages:any ={
+    intro:()=>{
+        return (
+            <>
+            <h1>Manage Passwords</h1>
+            <button
+            onClick={()=>{setState("start")}}
+            >Change State</button>
+          </>
+        )
+    },
+    start:()=>{
+        return (
+            <>
+            <h1>Start Passwords</h1>
+            <button
+            onClick={()=>{setState("intro")}}
+            >Change State</button>
+          </>
+        )
+    },
+    
+  }
+  const [state,setState] = useState("intro")
 
-  return (
-    <>
-      <h1>Manage Passwords</h1>
-    </>
-  );
+  return pages[state]()
 }
 
 export default ManagePasswords;
