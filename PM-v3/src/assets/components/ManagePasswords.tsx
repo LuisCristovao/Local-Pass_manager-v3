9feed75a07d35442173a9b43db2ef804ebf03558
Luis C <luis.cristovao92@gmail.com>
2025-04-15 10:43:18 +0100

    Update ManagePasswords.tsx

--- PM-v3/src/assets/components/ManagePasswords.tsx
+++ PM-v3/src/assets/components/ManagePasswords.tsx
@@ -1,4 +1,25 @@
+import { encrypt, decrypt, sha256 } from "../utils/cryptoUtils.ts";
+import { useEffect } from "react";
+
+const runCrypto = async () => {
+  const password = "my-password";
+  const secret = "Top Secret Data";
+
+  const encrypted = await encrypt(secret, password);
+  console.log("Encrypted:", encrypted);
+
+  const decrypted = await decrypt(encrypted, password);
+  console.log("Decrypted:", decrypted);
+
+  const hash = await sha256(secret);
+  console.log("SHA-256:", hash);
+};
+
 function ManagePasswords() {
+  useEffect(() => {
+    runCrypto();
+  },[]);
+  
   return (
     <>
       <h1>Manage Passwords</h1>
