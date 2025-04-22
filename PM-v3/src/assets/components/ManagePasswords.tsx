0f303c5efea87229a875cc521265d983de2ef720
Luis C <luis.cristovao92@gmail.com>
2025-04-22 15:52:28 +0100

    Update ManagePasswords.tsx

--- PM-v3/src/assets/components/ManagePasswords.tsx
+++ PM-v3/src/assets/components/ManagePasswords.tsx
@@ -4,6 +4,7 @@ import * as Crypto from "../utils/cryptoUtils";
 import "../css/ManagePasswords.css";
 import { useNavigate } from "react-router-dom"; // Add this import
 import * as Search from "../utils/searchUtils";
+import PassRecord from "./PassRecord";
 
 function ManagePasswords() {
   const [state, setState] = useState("intro");
