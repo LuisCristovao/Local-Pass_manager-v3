f47b63af504a5faba8a1da4484c7b569e1acaf5b
Luis C <luis.cristovao92@gmail.com>
2025-04-24 09:55:30 +0100

    Update vite.config.ts

--- PM-v3/vite.config.ts
+++ PM-v3/vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { viteSingleFile } from 'vite-plugin-singlefile';
-
+import { rename } from 'fs/promises'
+import { resolve } from 'path'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), viteSingleFile()],
+  plugins: [react(), viteSingleFile(),
+    {
+      name: 'rename-output',
+      async closeBundle() {
+        // Rename index.html to x.html after build
+        await rename(
+          resolve('dist', 'index.html'),
+          resolve('dist', 'PM_v3.html')
+        )
+      }
+    }
+  ]
+  
 })
