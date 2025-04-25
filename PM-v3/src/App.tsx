7fba80760aedf117d8ca40ae2b308659f5682447
Luis C <luis.cristovao92@gmail.com>
2025-04-25 10:51:56 +0100

    Update App.tsx
    copy to clip board and create json file

--- PM-v3/src/App.tsx
+++ PM-v3/src/App.tsx
@@ -9,6 +9,45 @@ function App() {
   const [subMenuSelected, setSubMenuSelected] = useState(0);
   const subMenuRef = useRef<any[]>([]);
 
+  function copyToClipboard(text: string) {
+    if (navigator.clipboard) {
+      navigator.clipboard
+        .writeText(text)
+        .then(() => {
+          console.log("Text copied to clipboard");
+        })
+        .catch((err) => {
+          console.error("Failed to copy text: ", err);
+        });
+    }
+  }
+  const downloadJsonAsFile = (jsonContent: {}, fileName: string) => {
+    // Create a Blob containing the JSON content
+    const blob = new Blob([JSON.stringify(jsonContent)], {
+      type: "application/json",
+    });
+
+    // Create a Blob URL for the Blob
+    const blobUrl = URL.createObjectURL(blob);
+
+    // Create an <a> element
+    const a = document.createElement("a");
+    a.href = blobUrl;
+    a.download = fileName || "data.json"; // Set the download attribute with a default file name if not provided
+
+    // Append the <a> element to the document
+    document.body.appendChild(a);
+
+    // Trigger a click event on the <a> element to simulate a download
+    a.click();
+
+    // Remove the <a> element from the document
+    document.body.removeChild(a);
+
+    // Optionally revoke the Blob URL after simulating the download
+    URL.revokeObjectURL(blobUrl);
+  };
+
   const menu_list = [
     {
       id: "manage",
@@ -43,18 +82,14 @@ function App() {
                     setSubMenuSelected(3);
                     subMenuRef.current = [
                       {
-                        id:"copy to clipboard",
-                        text:"copy to clipboard",
-                        onclick:()=>{
-
-                        }
+                        id: "copy to clipboard",
+                        text: "copy to clipboard",
+                        onclick: () => {},
                       },
                       {
-                        id:"save to encrypted file",
-                        text:"save to encrypted file",
-                        onclick:()=>{
-
-                        }
+                        id: "save to encrypted file",
+                        text: "save to encrypted file",
+                        onclick: () => {},
                       },
                     ];
                   },
@@ -64,7 +99,6 @@ function App() {
                   text: "Export Decrypted DB",
                   onclick: () => {
                     //needs to go to login page
-                    
                   },
                 },
               ];
@@ -78,7 +112,6 @@ function App() {
       text: "Sync Passwords",
       onclick: () => {
         //go to sync page
-        
       },
     },
     {
