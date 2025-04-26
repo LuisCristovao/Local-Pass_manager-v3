c8280d9c59b6cddfaf307c56227d2b2255a6c107
Luis C <luis.cristovao92@gmail.com>
2025-04-26 15:18:05 +0100

    Update ImportEncrypted.tsx

--- PM-v3/src/assets/components/ImportEncrypted.tsx
+++ PM-v3/src/assets/components/ImportEncrypted.tsx
@@ -3,80 +3,104 @@ import * as DB from "../utils/dbUtils";
 import * as Crypto from "../utils/cryptoUtils";
 import { useNavigate } from "react-router-dom"; // Add this import
 
-function ImportEncrypted(){
-
-    //ONLY for development/debugging
+function ImportEncrypted() {
+  //ONLY for development/debugging
   // if (typeof window !== "undefined") {
   //   (window as any).PM = { decryptedPasswords, Search };
   // }
 
-    const [success,setSuccess]= useState<string>("nothing")
+  const [success, setSuccess] = useState<string>("nothing");
 
-    
-    const  sync=async(encrypted_data_json:[])=>{
+  const overwrite = async (encrypted_data: string) => {
+    if (encrypted_data !== "") {
+      const encrypted_data_json: [] = JSON.parse(encrypted_data);
+      //clear db then append  records
+      try {
+        DB.clearDatabase();
 
-        if (await DB.databaseExists()){
-            //compare if the sync filed is the same and add new 
-            //update old records with new ones 
+        encrypted_data_json.forEach((record) => {
+          DB.add(record);
+        });
 
-
-        }else{
-            //import all records to database
-            encrypted_data_json.forEach(record=>{
-                DB.add(record)
-            })
-        }
-
-
-        return "success"
+        return "success";
+      } catch (err) {
+        return "error";
+      }
     }
-    const navigate = useNavigate();
+    return "nothing";
+  };
 
-    return (
-        <>
-        <button
-          style={{ position: "absolute", top: "10px", left: "10px" }}
-          onClick={() => {
-            navigate("/")
-          }}
-        >
-          Go back
-        </button>
-        <h1>Import Passwords</h1>
-        <textarea
+  const append = async (encrypted_data: string) => {
+    if (encrypted_data !== "") {
+      const encrypted_data_json: [] = JSON.parse(encrypted_data);
+      //append to the db the records
+      try {
+        encrypted_data_json.forEach((record) => {
+          DB.add(record);
+        });
+        return "success";
+      } catch (err) {
+        return "error";
+      }
+    }
+    return "nothing";
+  };
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <button
+        style={{ position: "absolute", top: "10px", left: "10px" }}
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        Go back
+      </button>
+      <h1>Import Passwords</h1>
+      <textarea
         id="import encrypted data"
         style={{
-            height:"350px",
-            width:"60%",
-            maxWidth:"800px",
-            minWidth:"300px",
-            marginBottom:"20px",
-            backgroundColor:"transparent",
-            fontSize:"1.5em"
+          height: "350px",
+          width: "60%",
+          maxWidth: "800px",
+          minWidth: "300px",
+          marginBottom: "20px",
+          backgroundColor: "transparent",
+          fontSize: "1.5em",
         }}
         placeholder="Insert Encrypted JSON here..."
-        ></textarea>
-        <div style={{
-            gap:"30px",
-            display:"flex"
-        }}>
-        <button
-        onClick={()=>{
-            // const textarea=document.getElementById("import encrypted data") as HTMLTextAreaElement
-            // setSuccess(()=>{
-            //     return sync(textarea.value)
-            // })
-            
+      ></textarea>
+      <div
+        style={{
+          gap: "30px",
+          display: "flex",
         }}
-        >Overwrite</button>
-        <button>Append</button>
-        </div>
-        
-        </>
-    )
-
-
-
-
+      >
+        <button
+          onClick={async () => {
+            const textarea = document.getElementById(
+              "import encrypted data"
+            ) as HTMLTextAreaElement;
+            const output = await overwrite(textarea.value);
+            setSuccess(output);
+          }}
+        >
+          Overwrite
+        </button>
+        <button
+          onClick={async () => {
+            const textarea = document.getElementById(
+              "import encrypted data"
+            ) as HTMLTextAreaElement;
+            const output = await append(textarea.value);
+            setSuccess(output);
+          }}
+        >
+          Append
+        </button>
+      </div>
+    </>
+  );
 }
 export default ImportEncrypted;
