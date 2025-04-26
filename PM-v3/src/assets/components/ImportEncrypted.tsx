4162a33a576bbc02a3fc0fe07a86c7c87f8a1649
Luis C <luis.cristovao92@gmail.com>
2025-04-26 14:59:25 +0100

    Update ImportEncrypted.tsx

--- PM-v3/src/assets/components/ImportEncrypted.tsx
+++ PM-v3/src/assets/components/ImportEncrypted.tsx
@@ -13,15 +13,35 @@ function ImportEncrypted(){
     const [success,setSuccess]= useState<string>("nothing")
 
     
-    const sync=(encrypted_data_json:{})=>{
+    const  sync=async(encrypted_data_json:[])=>{
+
+        if (await DB.databaseExists()){
+            //compare if the sync filed is the same and add new 
+            //update old records with new ones 
+
+
+        }else{
+            //import all records to database
+            encrypted_data_json.forEach(record=>{
+                DB.add(record)
+            })
+        }
 
 
         return "success"
     }
-
+    const navigate = useNavigate();
 
     return (
         <>
+        <button
+          style={{ position: "absolute", top: "10px", left: "10px" }}
+          onClick={() => {
+            navigate("/")
+          }}
+        >
+          Go back
+        </button>
         <h1>Import Passwords</h1>
         <textarea
         id="import encrypted data"
@@ -29,22 +49,29 @@ function ImportEncrypted(){
             height:"350px",
             width:"60%",
             maxWidth:"800px",
-            minWidth:"500px",
+            minWidth:"300px",
             marginBottom:"20px",
             backgroundColor:"transparent",
             fontSize:"1.5em"
         }}
         placeholder="Insert Encrypted JSON here..."
         ></textarea>
+        <div style={{
+            gap:"30px",
+            display:"flex"
+        }}>
         <button
         onClick={()=>{
-            const textarea=document.getElementById("import encrypted data") as HTMLTextAreaElement
-            setSuccess(()=>{
-                return sync(textarea.value)
-            })
+            // const textarea=document.getElementById("import encrypted data") as HTMLTextAreaElement
+            // setSuccess(()=>{
+            //     return sync(textarea.value)
+            // })
             
         }}
-        >Import</button>
+        >Overwrite</button>
+        <button>Append</button>
+        </div>
+        
         </>
     )
 
